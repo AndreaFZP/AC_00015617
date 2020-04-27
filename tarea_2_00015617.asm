9c496fe278e73d0b146215725ddb53118fd2bca9
AndreaFZP <AndreaFZP@users.noreply.github.com>
2020-04-26 22:54:48 -0600

    ej 3

--- tarea_2_00015617.asm
+++ tarea_2_00015617.asm
@@ -16,6 +16,15 @@ mov bx, 5d
 
 div bx
 
+section .text
+mov di, 0d
+mov cx, [len]
+lupi:  mov bl, [comnt+di] 
+       mov [di+200h],bl
+       inc	di
+       loop	lupi
+
+
 ;ejercicio 2
 
 mov ax, 2d
@@ -26,14 +35,40 @@ primero:
 	mul cx
 	mov [si+210h],ax
 	inc	si
+    cmp ax, 256d
 	jb primero
 
 segundo: 
     mul cx,
     mov [si+210h],ax
     inc si
+    cmp si, 14d
     jb segundo
 
 
+;ejercicio 3
+
+mov	cx, 0000h
+mov	si, 0d
+
+
+mov	ax, 0d
+mov	bx, 1d
+mov	[si+220h], ax
+inc	si
+mov	[si+220h], bx
+inc	si
+
+fibonacci:
+    mov	dx, bx
+	add	bx, ax
+	mov	[si+220h], bx
+	mov	ax, dx
+	inc	si
+	loop	fibonacci
 
 int 20h
+
+section .data
+comnt db "Me recupero"
+len equ $-comnt
